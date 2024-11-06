7bde6ab6959b93c5b3c1cfc455521e5ee3c06af3
Naila Jinan Gaisani <146344930+nellnelaa@users.noreply.github.com>
2024-11-06 13:51:04 +0700

    feature: create nav login (#1)

--- student-app/src/components/Navbar/index.jsx
+++ student-app/src/components/Navbar/index.jsx
@@ -1,36 +1,129 @@
-import { Link } from "@tanstack/react-router";
+import { Link, useNavigate } from "@tanstack/react-router";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setToken, setUser } from "../../redux/slices/auth";
+import { profile } from "../../service/auth";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Dropdown } from "react-bootstrap";
 
 const NavigationBar = () => {
-    return (
-        <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand as={Link} to="/">
-                    Student Wakanda App
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link as={Link} to="/">
-                            Home
-                        </Nav.Link>
-                    </Nav>
-                    <Nav>
-                            <>
-                                <Nav.Link as={Link} to="/login">
-                                    Login
-                                </Nav.Link>
-                                <Nav.Link as={Link} to="/register">
-                                    Register
-                                </Nav.Link>
-                            </>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { user, token } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    const getProfile = async () => {
+      // fetch get profile
+      const result = await profile();
+      if (result.success) {
+        // set the user state here
+        dispatch(setUser(result.data));
+        return;
+      }
+
+      // If not success
+      // delete the local storage here
+      dispatch(setUser(null));
+      dispatch(setToken(null));
+
+      // redirect to login
+      navigate({ to: "/login" });
+    };
+
+    if (token) {
+      // hit api auth get profile and pass the token to the function
+      getProfile();
+    }
+    if (!token) {
+      navigate({ to: "/login" });
+    }
+  }, [dispatch, navigate, token]);
+
+  const logout = (event) => {
+    event.preventDefault();
+
+    // delete the local storage here
+    dispatch(setUser(null));
+    dispatch(setToken(null));
+
+    // redirect to login
+    navigate({ to: "/login" });
+  };
+
+  return (
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          <img
+            src="/src/assets/images/logo.png"
+            alt=""
+            width="80"
+            height="32"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto"></Nav>
+          <Nav>
+            {user ? (
+              <>
+                <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
+
+                <Dropdown align="end" className="ms-3">
+                  <Dropdown.Toggle
+                    variant="link"
+                    id="dropdown-basic"
+                    className="d-flex align-items-center"
+                    style={{ textDecoration: "none", color: "inherit" }}
+                  >
+                    <Image
+                      src={user?.profile_picture}
+                      roundedCircle
+                      width="30"
+                      height="30"
+                      alt="User Profile"
+                      className="me-2"
+                    />
+                    {user?.name}
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item as={Link} to="/profile">
+                      Profile
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={logout}>Logout</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </>
+            ) : (
+              <>
+                {/* <Nav.Link as={Link} to="/login">
+                  Login
+                </Nav.Link> */}
+                {/* <Nav.Link as={Link} to="/register">
+                  Register
+                </Nav.Link> */}
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavigationBar;
--- student-app/src/routes/index.lazy.jsx
+++ student-app/src/routes/index.lazy.jsx
@@ -1,14 +1,62 @@
-import { createLazyFileRoute } from "@tanstack/react-router";
+import { createLazyFileRoute, useNavigate } from "@tanstack/react-router";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import SideNavigationBar from "../components/SideNav";
 
 export const Route = createLazyFileRoute("/")({
-    component: Index,
+  component: Index,
 });
 
 function Index() {
+  const { token } = useSelector((state) => state.auth);
+
+  const [students, setStudents] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getStudentData = async () => {
+      setIsLoading(true);
+      const result = await getStudents();
+      if (result.success) {
+        setStudents(result.data);
+      }
+      setIsLoading(false);
+    };
+
+    if (token) {
+      getStudentData();
+    }
+    if (!token) {
+      navigate({ to: "/login" });
+    }
+  }, [token, navigate]);
+
+  if (isLoading) {
     return (
-        <Row className="mt-4">
-            <h1>HELLO WORLD</h1>
-        </Row>
+      <Row className="mt-4">
+        <h1>Loading...</h1>
+      </Row>
     );
+  }
+
+  return (
+    <Row>
+      <Col>
+        <SideNavigationBar />
+      </Col>
+
+      <Col className="d-flex ">
+        {students.length === 0 ? (
+          <h1>Student data is not found!</h1>
+        ) : (
+          students.map((student) => (
+            <StudentItem student={student} key={student?.id} />
+          ))
+        )}
+      </Col>
+    </Row>
+  );
 }
