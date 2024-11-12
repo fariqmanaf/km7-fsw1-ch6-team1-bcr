333e6b812bbc8c57c9fc1e553976a104e7a37596
Fariq Manaf <145954724+fariqmanaf@users.noreply.github.com>
2024-11-13 01:21:17 +0700

    feat: update and delete for cars (#12)

--- student-app/src/service/cars/index.js
+++ student-app/src/service/cars/index.js
@@ -45,3 +45,34 @@ export const updateCar = async (id, car) => {
     const result = await response.json();
     return result;
 }
+
+export const createCar = async (car) => {  
+    const token = localStorage.getItem("token");
+    let url = `${import.meta.env.VITE_API_URL}/cars`;
+
+    const response = await fetch(url, {
+        headers: {
+            authorization: `Bearer ${token}`,
+        },
+        method: "POST",
+        body: car,
+    });
+
+    const result = await response.json();
+    return result;
+}
+
+export const deleteCar = async (id) => {
+    const token = localStorage.getItem("token");
+    let url = `${import.meta.env.VITE_API_URL}/cars/${id}`;
+
+    const response = await fetch(url, {
+        headers: {
+            authorization: `Bearer ${token}`,
+        },
+        method: "DELETE",
+    });
+
+    const result = await response.json();
+    return result;
+}
